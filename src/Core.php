c176981a4f5333a87d734f43102c24738ea20784
nplesa <nicu.plesa@net4media.net>
2022-07-22 17:26:17 +0300

    ..

--- src/Core.php
+++ src/Core.php
@@ -167,17 +167,6 @@
 
 	        return array_combine($listIds, $listVal);
 	    }
-	    protected function getInternalIP() {
-	    	$sock = socket_create(AF_INET, SOCK_DGRAM, SOL_UDP);
-			socket_connect($sock, "8.8.8.8", 53);
-			socket_getsockname($sock, $name); // $name passed by reference
-
-			// This is the local machine's external IP address
-			return $name;
-	    }
-	    protected function getExternalIP() {
-	    	
-	    }
 		public function getOS($user_agent = null) {
 			$memUsage = $this->getServerMemoryUsage(false);
 		    return [
