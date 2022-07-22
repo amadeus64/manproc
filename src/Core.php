cb999c8ba8894632ebc7aa40b17ab922cf7aac78
nplesa <nicu.plesa@net4media.net>
2022-07-22 13:28:41 +0300

    ..

--- src/Core.php
+++ src/Core.php
@@ -17,12 +17,36 @@
 
 		public function __construct()
 		{	
-
 			$this->_host = [
 				'simple'  => php_uname(),
-				'details' => PHP_OS
+				'details' => PHP_OS,
+				'full'	  => $this->getOSInformation()	
 			];	
 		}
+		protected function getOSInformation()
+	    {
+	        if (false == function_exists("shell_exec") || false == is_readable("/etc/os-release")) {
+	            return null;
+	        }
+
+	        $os         = shell_exec('cat /etc/os-release');
+	        $listIds    = preg_match_all('/.*=/', $os, $matchListIds);
+	        $listIds    = $matchListIds[0];
+
+	        $listVal    = preg_match_all('/=.*/', $os, $matchListVal);
+	        $listVal    = $matchListVal[0];
+
+	        array_walk($listIds, function(&$v, $k){
+	            $v = strtolower(str_replace('=', '', $v));
+	        });
+
+	        array_walk($listVal, function(&$v, $k){
+	            $v = preg_replace('/=|"/', '', $v);
+	        });
+
+	        return array_combine($listIds, $listVal);
+	    }
+
 		public function justDoIt() {
 	        return response()->json([
 			    'name' => 'Abigail',
