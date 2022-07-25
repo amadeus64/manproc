1ecf2c3af716d238236630c422e869ff04e845a4
nplesa <nicu.plesa@net4media.net>
2022-07-25 11:48:38 +0300

    ..

--- src/Core.php
+++ src/Core.php
@@ -15,7 +15,8 @@
 		protected $_id 				= 0;
 		protected $_log         	= [];
 		protected $_managedOptions  = ["auto","manual"];
-		protected $_managedTasks 	= [];
+		protected $_ManagedTasks 	= [];
+		protected $_nonManagedTasks = [];
 		protected $_started 		= false;
 		protected $_managed			= "auto";
 		protected $_host   			= [];
